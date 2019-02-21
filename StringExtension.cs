1e4a5e9f2ba7a65b8b2e959b86138e4fa7243cc0
Евгений Анищенко <anischenkozhenya@gmail.com>
2019-02-21 20:14:15 +0300

    Update StringExtension.cs

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,7 +6,23 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            char[] array = source.ToCharArray();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if ((array[i] == array[j])
+                        && (int)array[i] >= 0
+                        && (int)array[i] < 128
+                        && (int)array[j] >= 0
+                        && (int)array[j] < 128)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
         }
     }
 }
